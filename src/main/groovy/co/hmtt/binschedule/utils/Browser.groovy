461e6e62f0dac3cddf2d986d9b25a441618155c7
Stuart Wilson <stuart@hmtt.co.uk>
2020-01-21 21:24:09 +0000

    Remove docker containerization

--- src/main/groovy/co/hmtt/binschedule/utils/Browser.groovy
+++ src/main/groovy/co/hmtt/binschedule/utils/Browser.groovy
@@ -3,7 +3,6 @@ package co.hmtt.binschedule.utils
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 import java.util.concurrent.TimeUnit
@@ -11,15 +10,13 @@ import java.util.concurrent.TimeUnit
 @Component
 class Browser {
 
-    @Value('${wheelie.selenium.hub}')
-    private String hubUrl
-
-    def getBrowser() {
+    static def getBrowser() {
         System.setProperty('webdriver.chrome.driver', '/usr/bin/chromedriver')
-        ChromeOptions options = new ChromeOptions();
+        final ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        WebDriver driver = new ChromeDriver(options);
+        final WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+        driver
     }
 
 }
